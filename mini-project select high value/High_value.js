c33bd1c78d83fb7108442e218be2c8313b97c56d
Renanpacheco <prenan86@gmail.com>
2023-06-06 18:53:22 -0300

    addition of a test with empty array and alteration on page home to proceed with inputs of users

--- mini-project select high value/High_value.js
+++ mini-project select high value/High_value.js
@@ -17,7 +17,9 @@ function highValue (arrayOfValues){ // find the highest value in a array of valu
 
 }
 
-let result;
+
+// values used to test the function
+/*let result;
 let values= [0,15,4,7,1];
 console.log(values);
 result = highValue(values);
@@ -27,10 +29,16 @@ let values2= [-8,-15,-4,-1,-11];
 console.log(values2);
 result = highValue(values2);
 console.log(result);
+*/
+
+let empty= [];
+console.log(empty);
+result = highValue(empty);
+console.log(result);
 
 //future implementaion with a strings
-/*let values3= ['Test','Sarah','Biggest','none',123456789];
-console.log(values3);
-result = highValue(values3);
+/*let values4= ['Test','Sarah','Biggest','none'];
+console.log(values4);
+result = highValue(values4);
 console.log(result);
 */
