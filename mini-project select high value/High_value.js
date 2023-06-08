bf30651dedb5694b3c335ce588d943168cd13e9e
Renanpacheco <prenan86@gmail.com>
2023-06-08 18:15:50 -0300

    alteration to calculate with aleatoric values

--- mini-project select high value/High_value.js
+++ mini-project select high value/High_value.js
@@ -18,6 +18,52 @@ function highValue (arrayOfValues){ // find the highest value in a array of valu
 }
 
 
+function collectValue(e){
+    e.preventDefault();
+    let aux =document.getElementById("size_array");
+    let size = aux.value;
+    console.log(size)
+    //document.getElementById("capture").innerHTML = size;
+    
+}
+
+/*
+let size = Math.random()* (10 -1)+1;
+console.log(size);
+let values = [];
+
+//document.getElementById("capture").innerHTML = size;
+console.log(values);
+for (let i = 0; i < size; i++){
+    values.push(Math.random());
+    
+
+}
+
+console.log(values);
+console.log(highValue(values));
+*/
+
+
+
+
+/*
+let sizeOfArray = document.querySelector("#send");
+sizeOfArray.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    let entry= document.querySelector("#size_array");
+    //console.log(entry.value)
+
+    //let size= entry.value;
+    //console.log(size);
+
+});
+
+*/
+
+
 // values used to test the function
 /*let result;
 let values= [0,15,4,7,1];
@@ -29,13 +75,12 @@ let values2= [-8,-15,-4,-1,-11];
 console.log(values2);
 result = highValue(values2);
 console.log(result);
-*/
 
 let empty= [];
 console.log(empty);
 result = highValue(empty);
 console.log(result);
-
+*/
 //future implementaion with a strings
 /*let values4= ['Test','Sarah','Biggest','none'];
 console.log(values4);
